2563d574832a568e28200fbeb1c17a27fd23a189
elided-29 <elidedjunior@gmail.com>
2023-08-24 14:52:36 -0700

    handles

---
+++ func.c
@@ -0,0 +1,59 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * _printf - custom printf function
+ * @format: format
+ * Return: printed chars
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+
+	va_start(args, format);
+
+	int printed_chars = 0;
+	const char *c = format;
+
+	while (*c != '\0')
+	{
+		if (*c == '%')
+		{
+			c++;
+			if (*c == 'd' || *c == 'i')
+			{
+				int num = va_arg(args, int);
+
+				printed_chars += printf("%d", num);
+			}
+			else
+			{
+				putchar('%');
+				putchar(*c);
+				printed_chars += 2;
+			}
+		}
+		else
+		{
+			putchar(*c);
+			printed_chars++;
+		}
+		c++;
+	}
+	va_end(args);
+	return (printed_chars);
+}
+
+/**
+ * main - test the function
+ * Return: (0)
+ */
+
+int main(void)
+{
+	int num = 42;
+
+	_printf("This is a number: %d\n", num);
+	return (0);
+}
