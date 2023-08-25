b8c82f3b1760f4df85c6ed4d92994317166dd860
elided-29 <elidedjunior@gmail.com>
2023-08-24 17:32:20 -0700

    printf

--- print_function.c
+++ print_function.c
@@ -1,27 +1,30 @@
 #include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
 
 /**
- * _printf - printf function
- * @format: format
- * @return: printed char
+ * _printf - Custom printf function
+ * @format: Format string
+ * @...: Variable number of arguments
+ * Return: Number of characters printed
  */
 
 int _printf(const char *format, ...)
 {
-	int char_ = 0;
-	va_list list_of_arguments;
+	int printed_chars = 0;
+	va_list args;
 
 	if (format == NULL)
 		return (-1);
 
-	va_start(list_of_arguments, format);
+	va_start(args, format);
 
 	while (*format)
 	{
 		if (*format != '%')
 		{
 			write(1, format, 1);
-			char_++;
+			printed_chars++;
 		}
 		else
 		{
@@ -31,29 +34,41 @@ int _printf(const char *format, ...)
 			if (*format == '%')
 			{
 				write(1, format, 1);
-				char_++;
+				printed_chars++;
 			}
 			else if (*format == 'c')
 			{
-				char c = va_arg(list_of_arguments, int);
+				char c = va_arg(args, int);
 
 				write(1, &c, 1);
-				char_++;
+				printed_chars++;
 			}
 			else if (*format == 's')
 			{
-				char *str = va_arg(list_of_arguments, char*);
+				char *str = va_arg(args, char*);
 				int str_len = 0;
 
 				while (str[str_len] != '\0')
 					str_len++;
 				write(1, str, str_len);
-				char_ += str_len;
+				printed_chars += str_len;
 			}
 		}
 		format++;
 	}
-	va_end(list_of_arguments);
+	va_end(args);
+	return (printed_chars);
+}
+/**
+ * main - test function
+ * Return: (0)
+ */
 
-	return (char_);
+int main(void)
+{
+	char c = 'A';
+	char *str = "Hello, World!";
+
+	_printf("Character: %c\nString: %s\nLiteral %%\n", c, str);
+	return (0);
 }
