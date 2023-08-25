d483c4733a03334ce13847682c39d4655f2bcba4
elided-29 <elidedjunior@gmail.com>
2023-08-25 11:01:29 -0700

    printf

--- printf.c
+++ printf.c
@@ -1,66 +1,60 @@
-#include "main.c"
+#include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
+
 /**
- * _printf - prints a functions
- * @format: return format
- * Return: (0)
+ * _printf - Custom printf function
+ * @format: Format string
+ * @...: Variable number of arguments
+ * Return: Number of characters printed
  */
 
 int _printf(const char *format, ...)
 {
+	int printed_chars = 0;
+	va_list args;
+
 	if (format == NULL)
 		return (-1);
 
-	va_list args;
-
 	va_start(args, format);
 
-	int printed_chars = 0;
-
-	for (int i = 0; format[i] != '\0'; i++)
+	while (*format)
 	{
-		if (format[i] == '%')
+		if (*format != '%')
 		{
-			i++;  /*Move past '%'*/
-			if (format[i] == '\0')
+			write(1, format, 1);
+			printed_chars++;
+		}
+		else
+		{
+			format++;
+			if (*format == '\0')
 				break;
-			if (format[i] == 'c')
+			if (*format == '%')
 			{
-				char c = va_arg(args, int); /** 'char' is promoted to 'int' */
+				write(1, format, 1);
+				printed_chars++;
+			}
+			else if (*format == 'c')
+			{
+				char c = va_arg(args, int);
 
 				write(1, &c, 1);
 				printed_chars++;
 			}
-			else if (format[i] == 's')
+			else if (*format == 's')
 			{
-				char *str = va_arg(args, char *);
-
-				if (str == NULL)
-					str = "(null)";
-				int j = 0;
-
-				while (str[j] != '\0')
-				{
-					write(1, &str[j], 1);
-					j++;
-					printed_chars++;
-				}
-			}
-			else if (format[i] == '%')
-			{
-				write(1, "%", 1);
-				printed_chars++;
+				char *str = va_arg(args, char*);
+				int str_len = 0;
+				while (str[str_len] != '\0')
+					str_len++;
+				write(1, str, str_len);
+				printed_chars += str_len;
 			}
 		}
-		else
-		{
-			write(1, &format[i], 1);
-			printed_chars++;
-		}
+		format++;
 	}
-
 	va_end(args);
-
 	return (printed_chars);
 }
